de7e74571038e8e278977c2ee28f755807f595c5
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-03-27 17:03:13 -0400

    feat: wrapped connection to db in result

--- api/src/persistence.rs
+++ api/src/persistence.rs
@@ -4,17 +4,18 @@ use std::env;
 
 use crate::util;
 
-pub async fn connect() -> DatabaseConnection {
-    // TODO: Wrap in Result
-    let home_dir: String = env::var("HOME").expect("HOME env var not set.");
+pub async fn connect() -> Result<DatabaseConnection, String> {
+    let home_dir: String = env::var("HOME").map_err(|_| "HOME env var not set.")?;
     let path_to_db = format!("{}/{}", home_dir, String::from(".mypass/db.sqlite"));
     let db_url = format!("sqlite://{}", path_to_db);
-    let is_new = util::create_file(path_to_db.to_owned()).expect("");
+    let is_new = util::create_file(path_to_db.to_owned())?;
     let conn = Database::connect(db_url)
         .await
-        .expect("Failed to connect to store");
+        .map_err(|_| "Failed to connect to database")?;
     if is_new {
-        Migrator::up(&conn, None).await.unwrap();
+        Migrator::up(&conn, None)
+            .await
+            .map_err(|_| "Failed to push migration to database")?;
     }
-    conn
+    Ok(conn)
 }
