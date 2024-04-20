1f400c54bae7f1e270191ce9b447ad5277fca614
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-04-20 14:56:52 -0400

    ops: fetches latest tag automatically in install (#42)

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
 mkdir ~/.mypass/ 2>/dev/null | true
-
 cd ~/.mypass
 
 rm -rf src
 
-VERSION="0.2.1"
+latest_release=$(curl -sL "https://api.github.com/repos/GianlucaP106/mypass/releases/latest")
+latest_tag=$(echo "$latest_release" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-curl -fsSL https://github.com/GianlucaP106/mypass/archive/refs/tags/v${VERSION}.zip -o src.zip
+curl -fsSL https://github.com/GianlucaP106/mypass/archive/refs/tags/${latest_tag}.zip -o src.zip
 
 unzip -d src "src.zip"
 rm -rf "src.zip"
 
-cd "src/mypass-${VERSION}"
+tag_without_v=$(echo "$latest_tag" | tr -d "v")
+cd "src/mypass-${tag_without_v}"
 cargo build -r
 cp ./target/release/mypass ../..
